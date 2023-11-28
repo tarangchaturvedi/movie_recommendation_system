a02fd9b1cb87ff41104dde056a1cc0df46785beb
tarangchaturvedi <tarang.chaturvedi11@gmail.com>
2023-11-28 09:22:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def get_args():
     parser.add_argument("--mode", type=str)
     parser.add_argument("--train_file", type=str)
     parser.add_argument("--test_file", type=str)
+    parser.add_argument("--model", type=str)
     parser.add_argument("--prediction_file", type=str)
     args = parser.parse_args()
     return args
-
 
 if __name__ == "__main__":
     args = get_args()
@@ -21,8 +21,8 @@ if __name__ == "__main__":
         train_model(args.train_file)
     elif args.mode == "test":
         if args.prediction_file is not None:
-            test_model(args.test_file)
+            test_model(args.test_file, args.model)
         else:
-            test_model(args.test_file)
+            test_model(args.test_file, args.model)
     else:
         print("wrong mode")
